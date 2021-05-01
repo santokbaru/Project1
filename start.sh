44f206778a72cbd67e77c84ad5b6afaa396bb58c
santokbaru <83483235+santokbaru@users.noreply.github.com>
2021-05-01 09:03:09 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
-tar xf xmrig-6.12.1-linux-x64.tar.gz
-cd xmrig-6.12.1
 ./xmrig --url pool.hashvault.pro:80 --user TRTLuy8iwjndhZJ8RHeXPxXJbRozszAhbX7oL8tXx381EBoPYox66pTLS5kQR8ohALEXBmbRfYf61aqMPbdQwVYzS6wJLCsvU6i --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
